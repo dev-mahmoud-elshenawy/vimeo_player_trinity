3716ff65d7209ca608a0d8c6a0e758a888f3d0d8
Mahmoud El Shenawy <mahmoud.elshenawy@intcore.com>
2025-04-24 15:54:09 +0200

    Change vimeoLink which gets videos & Add Access Token for private content

--- lib/src/quality_links.dart
+++ lib/src/quality_links.dart
@@ -7,6 +7,7 @@ import "dart:collection";
 
 class QualityLinks {
   String? videoId;
+  String? accessToken;
 
   QualityLinks(this.videoId);
 
@@ -16,42 +17,31 @@ class QualityLinks {
 
   Future<SplayTreeMap?> getQualitiesAsync() async {
     try {
-      final vimeoLink =
-          Uri.tryParse('https://player.vimeo.com/video/${videoId!}/config');
-      var response = await http.get(vimeoLink!);
+      final Uri? vimeoLink =
+          Uri.tryParse('https://api.vimeo.com/videos/${videoId!}');
 
-      final jsonData = jsonDecode(response.body)['request']['files'];
-      final dashData = jsonData['dash'];
-      final hlsData = jsonData['hls'];
-      final defaultCDN = hlsData['default_cdn'];
-      final cdnVideoUrl = (hlsData['cdns'][defaultCDN]['url'] as String?) ?? '';
-      final rawStreamUrls =
-          (dashData['streams'] as List<dynamic>?) ?? <dynamic>[];
+      var response = await http.get(
+        vimeoLink!,
+        headers: {
+          "Accept": "application/vnd.vimeo.*+json;version=3.4",
+          if (accessToken != null) ...{
+            "Authorization": "bearer $accessToken",
+          }
+        },
+      );
+      var jsonDecoded = jsonDecode(response.body);
 
-      final sepList = cdnVideoUrl.split('/sep/video/');
-      final firstUrlPiece = sepList.firstOrNull ?? '';
-      final lastUrlPiece = ((sepList.lastOrNull ?? '').split('/').lastOrNull) ??
-          (sepList.lastOrNull ?? '');
+      log('Video Data: id -> $videoId | access token -> $accessToken');
 
-      final SplayTreeMap videoList = SplayTreeMap();
+      var jsonData = jsonDecoded['files'];
 
-      for (final item in rawStreamUrls) {
-        final urlId =
-            ((item['id'] ?? '') as String).split('-').firstOrNull ?? '';
+      SplayTreeMap videoList = SplayTreeMap.fromIterable(
+        jsonData,
+        key: (item) => "${item['rendition']} ${item['fps']}",
+        value: (item) => item['link'],
+      );
 
-        videoList.putIfAbsent(
-          "${item['quality']} ${item['fps']}",
-          () => '$firstUrlPiece/sep/video/$urlId/$lastUrlPiece',
-        );
-      }
-
-      if (videoList.isEmpty) {
-        videoList.putIfAbsent(
-          '720 30',
-          () => cdnVideoUrl,
-        );
-      }
-
+      log("video qualities ${videoList.values}");
       return videoList;
     } catch (error) {
       log('=====> REQUEST ERROR: $error');
